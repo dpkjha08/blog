53f67374aca4a490275d63b91d4522116693799d
Deepak Jha <dpkjha5337@gmail.com>
2021-09-02 18:57:27 +0530

    First Commit

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+var json = [
+    {
+        id: "1", 
+        topic: "Number Systems",
+        desc: "Kuch toh about Number Systems"
+    },
+    {
+        id: "2", 
+        topic: "Profit & Loss",
+        desc: "Kuch toh about Profit & Loss"
+    }
+];
---
+++ javascript/problems_on_trains_01.js
@@ -0,0 +1,15 @@
+var problems = [
+    {
+        id: "1",
+        question: "1. A train running at the speed of 60 km/hr crosses a pole in 9 seconds. What is the length of the train?",
+        option_a: "120 meters",
+        option_b: "180 meters",
+        option_c: "324 meters",
+        option_d: "150 meters",
+        image_present: "false",
+        image:"../images/kakashi.png",
+        ans: "This is some explanation"+
+            "wefewfewegw4",
+
+    }
+]
